61086c74b2d28e34b9eb66d0ee1170dc50da4b89
scottrbtrsn <scott.peter.robertson@gmail.com>
2018-10-12 09:18:35 -0500

    upgraded to resolve vulnerabilities and typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Your Cognitve Toolshed';
+  title = 'Your Cognitive Health Toolshed';
 }
