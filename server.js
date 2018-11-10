c5ab679a76da23f4d054d7d2bdadfbd8f1a545f8
scottrbtrsn <scott.peter.robertson@gmail.com>
2018-11-10 01:21:46 -0600

    trying again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/cognitve-health-ui'));
+app.use(express.static('./dist/cognitve-health-ui'));
 
 app.get('/*', function(req,res) {
     
